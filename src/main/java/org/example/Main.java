8883b2be3fdc34ae52ad6d30b2cad6e6da40c31a
Muhammad Naeem <mnaeem@gmail.com>
2026-01-13 00:16:19 -0800

    remote changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Main {
     public static void main(String[] args){
 
-        // Local changes
+        // remote changes
             WebDriver driver = new ChromeDriver();
             driver.manage().window().maximize();
             driver.get("https://demo.guru99.com/test/newtours/");
